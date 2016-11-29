fb1a68711288146df0fea06e35575c6a145ad6c0
Santi Nuñez <tau150@hotmail.com>
2016-11-28 21:27:06 -0300

    correccion de errores y se agreo comments

--- js/blog.js
+++ js/blog.js
@@ -1,138 +1,170 @@
-var BlogModule = (function(){
+var BlogModule = (function() {
 
-// Privado
+    // Privado
 
-var posts = [];
-    
-
+    var posts = [];
 
 
-function Post(title, body, id){
-    
-    var $li;
-    var $list = $('.sidebar-nav');
-    var $body;
-    var $bodyContainer = $('.bodyContainer');
 
-    this.title = title;
-    this.body = body;
-   
-    var id = composeId();
 
-  function composeId(){
-      var postsCount = posts.length;
-      if(postsCount == 0){
-        // Es el primer elemento, retorno 1
-        var id = 1;
-      }else{
-        // Busco el ultimo elemento (mayor id) y le sumo 1
-        var id = posts[postsCount - 1].getId() + 1
-      }
-      return id;
+    function Post(title, body, id) {
+
+        var $li;
+        var $list = $('.sidebar-nav');
+        var $body;
+        var $bodyContainer = $('.bodyContainer');
+
+        this.title = title;
+        this.body = body;
+
+        var id = composeId();
+        this.comments = [];
+
+
+
+        function composeId() {
+            var postsCount = posts.length;
+            if (!postsCount) {
+                // Es el primer elemento, retorno 1
+                var id = 1;
+            } else {
+                // Busco el ultimo elemento (mayor id) y le sumo 1
+                var id = posts[postsCount - 1].getId() + 1
+            }
+            return id;
+        }
+
+
+
+        this.render = function() {
+
+
+            newTitlePoints = this.title;
+
+
+            $li = $(
+                "<li data-id=" + id + " " + "data-title=" + newTitlePoints + ">" + newTitlePoints + "</li>"
+
+            );
+            $list.append($li);
+
+            // $body = $(
+            //     "<h1>" + this.title + "</h1>" +
+            //     "<p>" + this.body + "</p>" +
+            //     "<a href='#menu-toggle' class='btn btn-default' id='menu-toggle'>Toggle Menu</a>"
+            //  );
+
+            // $bodyContainer.append($body);
+
+        }
+
+        this.getId = function() {
+            return id;
+        }
+
+
     }
-  
 
-
-    this.render = function(){
-
-         if(this.title.length > 20){
-            
-            var newTitle = this.title.substr(0,20);
-            newTitlePoints =newTitle.concat('...');
-           
-         }else{
-             newTitlePoints = this.title;
-         }
-
-        $li = $(
-            "<li data-id=" + id + ">"  + newTitlePoints +"</li>" 
-         
-          );
-        $list.append($li);
-
-        // $body = $(
-        //     "<h1>" + this.title + "</h1>" +
-        //     "<p>" + this.body + "</p>" +
-        //     "<a href='#menu-toggle' class='btn btn-default' id='menu-toggle'>Toggle Menu</a>"
-        //  );
-        
-        // $bodyContainer.append($body);
-       
-      }
-
-        this.getId = function(){
-        return id;
-          }
-
-
-     }
+    function Comment(user, commentBody) {
+        this.user = user || 'anonimo';
+        this.commentBody = commentBody;
+        this.likes = 0;
+    }
 
 
 
 
 
-    function getPost(id){
-        var post = posts.find(function(post){
+    function getPost(id) {
+        var post = posts.find(function(post) {
             return post.getId(id) == id;
         });
 
         return post;
     }
 
+    function getAllPostsTags() {
+        var tags = [];
+        for (var i in posts) {
+            tags.push(posts[i].title);
+        }
+        return tags;
+    }
 
-// Publico
+    // Publico
 
-function createPost(title, body){
-    var post = new Post(title, body);
-    posts.push(post);
-    post.render();
-  }
-
-
-
-
- function deletePost(id){
-     var postToDelete = getPost(id);
-     
-     if(postToDelete){
-         var postIndex = posts.indexOf(id);
-         posts.splice(postIndex, 1);  
-     }
-
- };
+    function createPost(title, body) {
+        var post = new Post(title, body);
+        posts.push(post);
+        post.render();
+    }
 
 
-  function showPosts(){
-      for(var i in posts){
-          console.log(posts[i]);
-      }
-  }
-
-  function searchPost(id){
-      var resultPost = getPost(id);
-      return resultPost;
-  }
 
 
-  function updatePost(title,body,id){
-      var postToUpdate = getPost(id);
+    function deletePost(id) {
+        var postToDelete = getPost(id);
 
-      postToUpdate.body = body;
-      postToUpdate.title = title;
-  }
+        if (postToDelete) {
+            var postIndex = posts.indexOf(id);
+            posts.splice(postIndex, 1);
+        }
 
-var obj = {
-    createPost: createPost,
-    deletePost: deletePost,
-    showPosts: showPosts,
-    updatePost: updatePost,
-    searchPost: searchPost
+    };
 
-}
 
-return obj;
+    function showPosts() {
+        for (var i in posts) {
+            console.log(posts[i]);
+        }
+    }
+
+    function addComment(id, commentBody, user) {
+        var post = getPost(id);
+        var comment = new Comment(user, commentBody);
+        post.comments.push(comment);
+    }
+
+    function searchPost(id) {
+        var resultPost = getPost(id);
+        return resultPost;
+    }
+
+
+    function updatePost(title, body, id) {
+        var postToUpdate = getPost(id);
+
+        postToUpdate.body = body;
+        postToUpdate.title = title;
+    }
+
+    var obj = {
+        createPost: createPost,
+        deletePost: deletePost,
+        showPosts: showPosts,
+        updatePost: updatePost,
+        searchPost: searchPost,
+        addComment: addComment,
+        getAllPostsTags: getAllPostsTags
+
+    }
+
+    return obj;
 
 
 
 })();
 
+// BlogModule.createPost('prueba', 'cuerpo de prueba');
+// BlogModule.createPost('prueba2', 'cuerpo de prueba asfsaasaddasdasdsdsadsadsadasdsadsasf');
+// BlogModule.addComment(1, 'nuevo comentario', 'santi');
+
+// BlogModule.addComment(1, 'segundo comentario', 'agus');
+// BlogModule.addComment(1, 'tercer comentario');
+
+// BlogModule.addComment(2, 'asdsad', 'carlos');
+// BlogModule.addComment(2, 'lalalala', 'juan');
+
+
+
+// BlogModule.showPosts();
--- js/script.js
+++ js/script.js
@@ -1,138 +1,253 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-$( function() {
-    $( "#selectable" ).selectable();
- 
-  } );
+    var cuerpoPrincipal = $('.introductionContainer');
+
+    var $inputsComments = $('.inputsComments');
+
+    var tags = BlogModule.getAllPostsTags();
+
+    $(function() {
+
+        $("#tags").autocomplete({
+
+
+            source: tags,
+            select: function(event, ui) {
+                var title = ui.item.value;
+                var list = $('.sidebar-nav li');
+
+                $.each(list, function(index, value) {
+                    if ($(value).data('title') === title) {
+
+                        $(this).addClass('ui-selected');
+
+                        var htmlComments = $('.container-comments .comment').html();
+
+                        if (htmlComments) {
+                            $('.container-comments .comment').empty();
+                        }
+
+                        var idToAppend = $(this).data('id');
+
+                        var bodyToAppend = BlogModule.searchPost(idToAppend);
+                        bodyToAppendText = bodyToAppend.body;
+
+                        var titleToAppend = $(this).text();
+                        $('.estados').hide();
+                        $('.introductionContainer h1').text(titleToAppend);
+                        $('.introductionContainer .instructions').html(bodyToAppendText);
+                        $('.addCommentModal').show();
+                        $('.container-comments').show();
+                        setComments(idToAppend);
+                        $('.container-comment:even').css("background", "#F7F6F5");
 
 
 
-$('li').hover(function(){
-    $(this).css('cursor','pointer');
-
-});
-
-var cuerpoPrincipal = $('.introductionContainer');
+                    }
 
 
+                });
+            }
+        });
+    });
 
-    
 
-$.get( "https://jsonplaceholder.typicode.com/posts", function( data ) {
-    for(var i in data){
-         BlogModule.createPost(data[i].title, data[i].body);
+
+
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(function() {
+        $("#selectable").selectable();
+
+    });
+
+    function setComments(id) {
+        $('.container-comments .comment').empty();
+        var post = BlogModule.searchPost(id);
+        var commentsToAppend = post.comments;
+        $.each(commentsToAppend, function(index, value) {
+
+
+            $('.container-comments .comment').append("<div class='container-comment'><p class='userComment'> Usuario: " + value.user + "</p><p>" + value.commentBody + "</p></div>");
+
+        });
 
     }
-});
-    
 
 
-  $('.addPost').click(function(){
-        var postTitle =  $('#titleInput').val();
-        var postBody = $('textarea').val();
 
-        if(postTitle === '' || postBody===''){
+
+
+
+
+    // $.get("https://jsonplaceholder.typicode.com/posts/", function(data) {
+    //     for (var i in data) {
+    //         BlogModule.createPost(data[i].title, data[i].body);
+
+    //     }
+    // });
+
+
+
+
+    $('#addPost').click(function() {
+
+        cleanWrap();
+        var postTitle = $('#titleInput').val();
+        // var postBody = $('textarea').val();
+        var postBody = tinyMCE.activeEditor.getContent();
+
+        if (!postTitle || !postBody) {
             $.growl.error({ message: "Ambos campos son obligatorios" });
-            return; 
+            return;
         }
 
         BlogModule.createPost(postTitle, postBody);
-        // console.log(BlogModule.showPosts());
+
         $('#myModal input').val('');
         $('#myModal textarea').val('');
         $('#myModal').modal('toggle');
+        tinyMCE.activeEditor.setContent('');
+
     });
 
 
-    $('#btn-delete').click(function(){
-       var selectedElements = $("li.ui-selected");
-       $.each(selectedElements, function(i, el){
-          var idElement =  $(el).data('id');
+    $('#btn-delete').click(function() {
+        var selectedElements = $("li.ui-selected");
+        $.each(selectedElements, function(i, el) {
+            var idElement = $(el).data('id');
 
-         BlogModule.deletePost(idElement);
-         $(el).remove();
-         
-       });
+            BlogModule.deletePost(idElement);
+            $(el).remove();
+
+        });
         cleanWrap();
     });
 
- 
-   $('h3').click(function(){
-       cleanWrap();
-   });
-    
+
+    $('h3').click(function() {
+        cleanWrap();
+    });
 
 
-    function cleanWrap(){
+
+    function cleanWrap() {
         $('.introductionContainer h1').text('Bienvenido');
         $('.instructions').text('Selecciona el posta que desees ver de la lista de la izquierda');
         $('.estados').show();
+        $("li").removeClass("ui-selected");
+        $('.container-comments').hide();
+        $('.addCommentModal').hide();
+
     }
 
 
 
-    $('#btn-edit').click(function(){
-           $(".modalEdit").attr('id','myModalEdit');
-           var selectedElements = $("li.ui-selected");
-           var selection = selectedElements[0];
-           if(selectedElements.length > 1){
-                 $("#myModalEdit").removeAttr('id');
-                $.growl.error({ message: "No puedes editar mas de un campo a la vez" });
-                  
+    $('#btn-edit').click(function() {
+        $(".modalEdit").attr('id', 'myModalEdit');
+        var selectedElements = $("li.ui-selected");
+        var selection = selectedElements[0];
+        if (selectedElements.length > 1) {
+            $("#myModalEdit").removeAttr('id');
+            $.growl.error({ message: "No puedes editar mas de un campo a la vez" });
+            return;
+        }
 
-                return ;
-                
-           }
+        var idPost = $("li.ui-selected").data('id');
+        var post = BlogModule.searchPost(idPost);
+        bodyToAppendText = post.body;
+        $('#titleInputEdit').val(post.title);
 
-           var idPost = $("li.ui-selected").data('id');
-           var post = BlogModule.searchPost(idPost);
-           $('#titleInputEdit').val(post.title);
-           $('#bodyInputEdit').val(post.body);
-
-          
-
-     });
-
-     $('#btn-sort').click(function(){
-        var ordenado = false;
-        var listado = $('.sidebar-nav');
-	    var elementos = listado.children("li").get();
-	    elementos.sort(function(a,b) {
-		var A = $(a).text().toUpperCase();
-		var B = $(b).text().toUpperCase();
- 		return (A < B) ? -1 : (A > B) ? 1 : 0;
-	});
-	    $.each(elementos, function(id, elemento) {
-		listado.append(elemento);
-	    });
-     });
+        //  tinyMCE.activeEditor.setContent('<p>some</p>');
 
 
-  $('.savePost').click(function(){
-            var idPost = $("li.ui-selected").data('id');
-            var newTitle = $('#titleInputEdit').val();
-            var newBody = $('#bodyInputEdit').val();
-            BlogModule.updatePost(newTitle, newBody,idPost);
-            cleanWrap();
-            $("li.ui-selected").text(newTitle);
-            $("li.ui-selected").removeClass('ui-selected');
-            $('#myModalEdit').modal('toggle');
+      
+        $('#myModalEdit').on('shown.bs.modal', function(e) {
+             setTimeout(tinyMCE.activeEditor.setContent(bodyToAppendText), 100);
+        })
 
-            console.log(BlogModule.showPosts());
-
-     });
-
-    $('.sidebar-nav').on('click', 'li',function(){
-       var idToAppend = $(this).data('id');
-
-       var bodyToAppend = BlogModule.searchPost(idToAppend);
-       var titleToAppend= $(this).text();
-       $('.estados').hide();
-       $('.introductionContainer h1').text(titleToAppend);
-       $('.introductionContainer .instructions').text(bodyToAppend.body);
     });
 
- 
+    $('#btn-sort').click(function() {
+        var ordenado = false;
+        var listado = $('.sidebar-nav');
+        var elementos = listado.children("li").get();
+        elementos.sort(function(a, b) {
+            var A = $(a).text().toUpperCase();
+            var B = $(b).text().toUpperCase();
+            return (A < B) ? -1 : (A > B) ? 1 : 0;
+        });
+        $.each(elementos, function(id, elemento) {
+            listado.append(elemento);
+        });
+    });
+
+
+    $('#savePost').click(function() {
+        var idPost = $("li.ui-selected").data('id');
+        var newTitle = $('#titleInputEdit').val();
+        var newBody = $('#bodyInputEdit').val();
+        BlogModule.updatePost(newTitle, newBody, idPost);
+        cleanWrap();
+        $("li.ui-selected").text(newTitle);
+        $("li.ui-selected").removeClass('ui-selected');
+        $('#myModalEdit').modal('toggle');
+
+    });
+
+
+
+    $('.sidebar-nav').on('click', 'li', function() {
+        var htmlComments = $('.container-comments .comment').html();
+
+        if (htmlComments) {
+            $('.container-comments .comment').empty();
+        }
+
+        var idToAppend = $(this).data('id');
+
+        var bodyToAppend = BlogModule.searchPost(idToAppend);
+        bodyToAppendText = bodyToAppend.body;
+
+        var titleToAppend = $(this).text();
+        $('.estados').hide();
+        $('.introductionContainer h1').text(titleToAppend);
+        $('.introductionContainer .instructions').html(bodyToAppendText);
+        $('.addCommentModal').show();
+        $('.container-comments').show();
+        setComments(idToAppend);
+        $('.container-comment:even').css("background", "#F7F6F5");
+    });
+
+
+
+
+    $('.addCommentModal').click(function() {
+        $('#userInput').val('');
+        tinyMCE.activeEditor.setContent('');
+
+    });
+
+    $('#addComment').click(function() {
+
+        var idToComment = $('.ui-selected').data('id');
+        var user = $('#userInput').val();
+        var comment = tinyMCE.activeEditor.getContent();
+
+        if (!comment) {
+            $.growl.error({ message: "El comentario no puede estar vacio" });
+            return;
+        }
+        BlogModule.addComment(idToComment, comment, user);
+
+        $('#commentModal').modal('toggle');
+        setComments(idToComment);
+
+    });
 
 });
